125b35106997ac900c3ad438f7f8371c25aa046f
RobertAbabei <robert_ababei97@yahoo.com>
2017-01-11 23:12:58 +0200

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,24 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <conio.h>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <Windows.h>
+
+
+using namespace std;
+
+
+
+// TODO: reference additional headers your program requires here
